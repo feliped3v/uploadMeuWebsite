1ae9e5772601e485963150ed504e724f0a5237dc
Felipe Ferreira <felipedoc.a77@gmai.com>
2021-06-08 00:18:36 -0300

    Primeiro Projeto

---
+++ js/main.js
@@ -0,0 +1,51 @@
+jQuery(document).ready(function ($) {
+    //FIXED HEADER
+    window.onscroll = function () {
+        if (window.pageYOffset > 140) {
+            $("#header").addClass("active");
+        } else {
+            $("#header").removeClass("active");
+        }
+    };
+
+    //ISOTOPE
+    let btns = $("#servicos .button-group button");
+
+    btns.click(function (e) {
+        $("#servicos .button-group button").removeClass("active");
+        e.target.classList.add("active");
+
+        let selector = $(e.target).attr("data-filter");
+        $("#servicos .grid").isotope({
+            filter: selector,
+        });
+    });
+
+    $(window).on("load", function () {
+        $("#servicos .grid").isotope({
+            filter: "*",
+        });
+    });
+
+    //OWL
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        dots: true,
+        lazyLoad: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 2,
+            },
+        },
+    });
+});
